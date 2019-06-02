0245034ddb512f276aa268ed41419e07097a48d4
sansx <xiathxth@gmail.com>
2019-06-02 13:39:48 +0800

    :art: new code

--- jd-scroll/js/index.js
+++ jd-scroll/js/index.js
@@ -1,34 +1,21 @@
-$('.btntop').on('click', function (event) {
-  event.preventDefault();
-  return $('html, body').animate({
-    scrollTop: 0
-  }, 300);
-  // return false;
-})
 var titles = ["名家推荐", "艺术收藏", "好物好价", "甄选优汇", "人气推荐", "艺品指南", "好物好价"];
 // var infoBox = [...Object.keys(titles)].map(res => res.repeat(res * 1 + 1));
 var infoBox = ["名家推荐家推荐", "艺术收藏2", "好物好价3", "甄选优汇4", "人气推荐5", "艺品指南6", "好物好价格7"];
 var isAnime = false
 var list = $(".list");
+var listBox = $(".flexBox")
 var isIE = checkIE()
 var listItems = function () {
   return isIE ? $(".IE_list span") : $(".list span")
 };
-var listBox = $(".flexBox")
 
-function checkIE() {
-  if (!!window.ActiveXObject || "ActiveXObject" in window) {
-    $(".flexBox").addClass("IEBox").removeClass("flexBox")
-    $(".list").addClass("IE_list").removeClass("list")
-    $(".btnBox").addClass("IE_btnBox").removeClass("btnBox")
-    $(".scrollBox").css("margin", "0 auto")
-    return true;
-  } else {
-    $("body").append("<script src='https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js'></script>");
-    return false;
-  }
-}
-
+// $('.btntop').on('click', function (event) {
+//   event.preventDefault();
+//   return $('html, body').animate({
+//     scrollTop: 0
+//   }, 300);
+//   // return false;
+// })
 
 window.onload = function () {
   var titlesCon = "";
@@ -49,7 +36,20 @@ window.onload = function () {
   })
 };
 
-var IE_spanFn = function (el) {
+function checkIE() {
+  if (!!window.ActiveXObject || "ActiveXObject" in window) {
+    $(".flexBox").addClass("IEBox").removeClass("flexBox")
+    $(".list").addClass("IE_list").removeClass("list")
+    $(".btnBox").addClass("IE_btnBox").removeClass("btnBox")
+    $(".scrollBox").css("margin", "0 auto")
+    return true;
+  } else {
+    $("body").append("<script src='https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.min.js'></script>");
+    return false;
+  }
+}
+
+function IE_spanFn(el) {
   var target = $(el.currentTarget);
   var num = target.attr("index");
   if (num === 0) return
@@ -63,32 +63,27 @@ var IE_spanFn = function (el) {
   indexSpan()
 }
 
-var spanFn = function (el) {
+function spanFn(el) {
   if (isAnime) return
   isAnime = true
   var target = $(el.currentTarget);
+  var cloneBox = [];
   var num = target.attr("index");
-  setAll(target.attr("key"));
   var spanWidth = parseFloat(
     listItems()
     .eq(0)
     .css("width")
   );
+  setAll(target.attr("key"));
   listItems()
     .removeClass("centerTitle")
     .addClass("smallFt");
   target.addClass("centerTitle");
   if (num === 0) return;
-  var cloneBox = [];
-  num > 0 ?
-    target.css({
-      left: "none",
-      right: 0
-    }) :
-    target.css({
-      right: "none",
-      left: 0
-    });
+  target.css({
+    left: num > 0 && 0,
+    right: num > 0 || 0
+  })
   for (var i = 0; i < Math.abs(num); i++) {
     cloneBox.push(
       listItems()
@@ -98,18 +93,15 @@ var spanFn = function (el) {
   }
   cloneBox.forEach(function (el) {
     el.on("click", spanFn);
-    num < 0 ? list.append(el) : list.prepend(el);
+    list[num < 0 ? "append" : "prepend"](el)
   });
-  var moveArr = [0, -Math.abs(num * spanWidth)];
   var obj = {
     targets: ".list",
     duration: 300,
     easing: "easeInOutQuad",
     update: function (anim) {
       if (anim.progress < 30) return;
-      if (listItems().hasClass("smallFt")) {
-        listItems().removeClass("smallFt");
-      }
+      listItems().hasClass("smallFt") && listItems().removeClass("smallFt");
     },
     complete: function () {
       for (var i = 0; i < Math.abs(num); i++) {
@@ -125,19 +117,20 @@ var spanFn = function (el) {
       isAnime = false
     }
   };
-  obj[num < 0 ?"left":"right"] = moveArr
+  var moveArr = [0, -Math.abs(num * spanWidth)];
+  obj[num < 0 ? "left" : "right"] = moveArr
   anime && anime(obj);
 };
 
 function indexSpan() {
   listItems().each(function (index, el) {
-    $(el).attr("index", Math.floor(listItems().length / 2) - index);
+    $(el).attr("index", getMidNum(listItems(), '') - index);
   });
 };
 
 function getCenter() {
   listItems()
-    .eq(Math.floor(listItems().length / 2))
+    .eq(getMidNum(listItems(), ''))
     .addClass("centerTitle");
 }
 
@@ -147,19 +140,25 @@ function centerInfo(num) {
 
 function setContain(num) {
   $(".contain .items").css("display", "none")
-  $(".contain .items").eq(num).css("display", "block")
+    .eq(num).css("display", "block")
 }
 
 function changeBg(num) {
   // $(".changebg .items").css("display","none")
-  $(".changebg .items").eq(num).addClass('_2ZxbGWxwHnyspJNFD6yxAU').siblings().removeClass("_2ZxbGWxwHnyspJNFD6yxAU");
+  $(".changebg .items").eq(num)
+    .addClass('_2ZxbGWxwHnyspJNFD6yxAU')
+    .siblings().removeClass("_2ZxbGWxwHnyspJNFD6yxAU");
 }
 
 function setAll(num) {
   var infoKey = listItems()
-    .eq(Math.floor(listItems().length / 2))
+    .eq(getMidNum(listItems(), ''))
     .attr("key");
   centerInfo(num || infoKey)
   setContain(num || infoKey)
   changeBg(num || infoKey)
+}
+
+function getMidNum(target, type) {
+  return target.length && Math[type || "floor"](target.length / 2)
 }
